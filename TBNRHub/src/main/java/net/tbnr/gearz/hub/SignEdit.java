f90e86c9a566b9e0deba6078dc68f1ffba7c2be2
Igor Randjelovic <rigor789@gmail.com>
2014-01-10 20:58:52 +0100

    Added a test command for the magic sign.

--- TBNRHub/src/main/java/net/tbnr/gearz/hub/SignEdit.java
+++ TBNRHub/src/main/java/net/tbnr/gearz/hub/SignEdit.java
@@ -1,26 +1,40 @@
 package net.tbnr.gearz.hub;
 
+import net.tbnr.util.command.TCommand;
+import net.tbnr.util.command.TCommandHandler;
+import net.tbnr.util.command.TCommandSender;
+import net.tbnr.util.command.TCommandStatus;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.block.Sign;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.block.SignChangeEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.HashMap;
 
 /**
  * Created by rigor789 on 2014.01.10..
  */
-public class SignEdit implements Listener {
+public class SignEdit implements Listener, TCommandHandler {
 
     private HashMap<String, Sign> players;
+    private String name;
 
     public SignEdit(){
         this.players = new HashMap<>();
+        this.name = ChatColor.AQUA + "The magic SIGN!!!!!";
     }
 
     @EventHandler
     public void onSignPlace(BlockPlaceEvent event){
+        if(!event.getPlayer().getItemInHand().getItemMeta().getDisplayName().equalsIgnoreCase(this.name)) return;
         if(!(event.getBlockPlaced().getState() instanceof Sign)) return;
         if(!(event.getBlockAgainst().getState() instanceof Sign)) return;
         Sign sign = (Sign) event.getBlockAgainst().getState();
@@ -41,4 +55,23 @@ public class SignEdit implements Listener {
         this.players.remove(event.getPlayer().getName());
     }
 
+    @TCommand(
+            name = "magicsign",
+            usage = "/magicsign",
+            permission = "gearz.magicsign",
+            senders = { TCommandSender.Player }
+    )
+    public TCommandStatus magicsign(CommandSender sender, TCommandSender type, TCommand meta, Command command, String[] args) {
+        Player player = (Player) sender;
+        ItemStack magic = new ItemStack(Material.WALL_SIGN);
+        ItemMeta itemMeta = magic.getItemMeta();
+        itemMeta.setDisplayName(name);
+        player.getInventory().addItem(magic);
+        return TCommandStatus.SUCCESSFUL;
+    }
+
+    @Override
+    public void handleCommandStatus(TCommandStatus status, CommandSender sender, TCommandSender senderType) {
+        TBNRHub.handleCommandStatus(status, sender);
+    }
 }
