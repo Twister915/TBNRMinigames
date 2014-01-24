61df3bd78b493f1eae2e907a331c641a8291fe5d
georgePadolsey <georgepadolsey@gmail.com>
2014-01-24 13:36:41 +0000

    fixed one more unused import ;)

--- TBNRHub/src/main/java/net/tbnr/gearz/hub/ToggleItem.java
+++ TBNRHub/src/main/java/net/tbnr/gearz/hub/ToggleItem.java
@@ -4,7 +4,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 /**
