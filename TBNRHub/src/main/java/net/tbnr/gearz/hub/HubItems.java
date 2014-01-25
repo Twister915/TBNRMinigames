83cd7801247ce67030676bedf6c92c229a83a1f7
georgePadolsey <georgepadolsey@gmail.com>
2014-01-25 20:30:23 +0000

    Removing Logging
    ALL FIXED
    :D

--- TBNRHub/src/main/java/net/tbnr/gearz/hub/HubItems.java
+++ TBNRHub/src/main/java/net/tbnr/gearz/hub/HubItems.java
@@ -78,10 +78,8 @@ public class HubItems implements Listener {
 
     private boolean shouldAdd(Player player, List<ItemStack> item) {
 	    for(ItemStack i: item) {
-		    TBNRHub.getInstance().getLogger().info("testing"+i.getType().toString());
 		    if(player.getInventory().contains(i)) return false;
 	    }
-	    TBNRHub.getInstance().getLogger().info("true");
 	    return true;
     }
 }
