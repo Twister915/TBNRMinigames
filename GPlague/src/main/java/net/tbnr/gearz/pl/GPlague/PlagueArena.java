efacd12c8b21e11db279fe13d92e14ae2d39c4c3
georgePadolsey <georgepadolsey@gmail.com>
2014-01-27 21:32:35 +0000

    another commit

--- GPlague/src/main/java/net/tbnr/gearz/pl/GPlague/PlagueArena.java
+++ GPlague/src/main/java/net/tbnr/gearz/pl/GPlague/PlagueArena.java
@@ -15,8 +15,12 @@ import org.bukkit.World;
  */
 public class PlagueArena extends Arena {
 
-	@ArenaField(key = "spawn-points", loop = true, type = ArenaField.PointType.Player, longName = "Spawn Points")
-	public ArenaIterator<Point> spawnPoints;
+	@ArenaField(key = "human-spawn", loop = true, type = ArenaField.PointType.Player, longName = "Human Spawn Points")
+	public ArenaIterator<Point> humanSpawnPoints;
+
+	@ArenaField(key = "zombie-spawn", loop = true, type = ArenaField.PointType.Player, longName = "Zombie Spawn Points")
+	public ArenaIterator<Point> zombieSpawnPoints;
+
 
 	public PlagueArena(String name, String author, String description, String worldId, String id) {
 		super(name, author, description, worldId, id);
