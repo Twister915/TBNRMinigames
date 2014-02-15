3c78979b23db0c6e2def2f737b204c0e9e44986e
georgePadolsey <georgepadolsey@gmail.com>
2014-02-15 22:31:12 +0000

    Commit for testing plague

--- GPlague/src/main/java/net/tbnr/gearz/pl/PlagueGame.java
+++ GPlague/src/main/java/net/tbnr/gearz/pl/PlagueGame.java
@@ -44,7 +44,7 @@ import java.util.*;
 				"An infected win when everyone is infected, humans win when all the infected are cured." +
 				"A poisoned player will count as infected for this purpose (meaning there must be no poison or infected present for the game to end)",
 		key = "plague",
-		minPlayers = 6,
+		minPlayers = 2,
 		maxPlayers = 28,
 		mainColor = ChatColor.GREEN,
 		secondaryColor = ChatColor.GOLD)
